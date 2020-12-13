0bac56fb2565bafb31a43ca992efe091a49ea905
faran4engg <faran4engg@gmail.com>
2020-12-13 13:04:19 +0100

    some cleanup

--- src/domains/Todos/components/todo-card/TodoCard.tsx
+++ src/domains/Todos/components/todo-card/TodoCard.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 const TodoCard = ({ isLoading, todos }) => (
   <div className="flex items-center">
-    <span>check console</span>
+    <p>Check Browser's Redux Dev Tool</p>
   </div>
 );
 
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,25 @@
 import { ForbiddenIcon } from '@assets/icons';
+import { Link } from 'react-router-dom';
 
 const Page404 = () => (
   <div className="flex flex-col items-center">
     <ForbiddenIcon
-      className="w-12 h-12 mt-8 text-purple-200"
+      className="w-12 h-12 mt-8 text-purple-600"
       aria-hidden="true"
     />
     <h1 className="text-6xl font-semibold text-gray-700 dark:text-gray-200">
       404
     </h1>
-    <p className="text-gray-700 dark:text-gray-300">
-      Page not found. Check the address or{' '}
-      <a
-        className="text-purple-600 hover:underline dark:text-purple-300"
-        href="../index.html"
-      >
-        go back
-      </a>
-      .
+    <p className="text-gray-700 dark:text-gray-300 my-4">
+      Page not found. Check the address or
     </p>
+
+    <Link
+      className="animate-bounce text-purple-600 hover:underline dark:text-purple-300"
+      to="/"
+    >
+      Go to Home page
+    </Link>
   </div>
 );
 
